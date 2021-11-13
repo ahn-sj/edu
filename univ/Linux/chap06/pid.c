8e20debf5f71c2b8901a62632c340db539f29227
ahn-sj <jaesa5221@gmail.com>
2021-11-13 15:30:38 +0900

    delete folder univ-linux(system programming)

--- univ/Linux/chap06/pid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* print process number*/
-int main()
-{
-    int pid;
-    printf("my process num : [%d] \n", getpid());
-    printf("my p process num : [%d] \n", getppid());
-}
